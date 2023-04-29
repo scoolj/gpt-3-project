f50c97517c5f5d2621de40e17b210c09cd2f5ce8
Scoolj <scoolj99@gmail.com>
2023-04-29 15:09:05 +0100

    blog, possibility, feature,and  cta  sections  done

--- src/containers/footer/Footer.jsx
+++ src/containers/footer/Footer.jsx
@@ -3,7 +3,10 @@ import './footer.css';
 
 const Footer = () => {
   return (
-    <div>
+    <div className='gpt3__footer section__padding'>
+        <div className='gpt3__footer-heading'>
+            <h1 className='gradient__text'> </h1>
+        </div>
       Footer
     </div>
   )
